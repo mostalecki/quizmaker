ed4cf3bf2c61d6be43d86a7a09ffa87283e967e4
Mateusz <1b13ostalecki@gmail.com>
2019-04-13 01:13:36 +0200

    Add custom AnswerControl

---
+++ QuizMaker/Views/AnswerControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizMaker
+{
+    public partial class AnswerControl : UserControl
+    {
+        public string Text
+        {
+            get
+            {
+                return textBoxAnswer.Text;
+            }
+            set
+            {
+                textBoxAnswer.Text = value;
+            }
+        }
+        public bool IsCorrect
+        {
+            get
+            {
+                return checkBoxIsCorrect.Checked;
+            }
+            set
+            {
+                checkBoxIsCorrect.Checked = value;
+            }
+        }
+        public AnswerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
